5a477e8dc11f37eb3ac7fc0e80128716e42bb56a
felipe-1945 <felipe.mendes.batista@escola.pr.gov.br>
2025-08-06 12:39:04 -0300

    Create perguntas.js

---
+++ assets/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+  'Contrabaixo',
+  'Qual a função principal do contrabaixo em uma banda?',
+  'Marcar a linha de baixo e sustentar a harmonia.'
+)
+
+criaCartao(
+  'Baixo Elétrico',
+  'Quantas cordas tem um baixo padrão?',
+  'Geralmente possui 4 cordas.'
+)
+
+criaCartao(
+  'Técnica',
+  'O que é "slap" no baixo?',
+  'É uma técnica de percussão feita com o polegar.'
+)
+
+criaCartao(
+  'História',
+  'Quem é considerado o "pai do baixo elétrico"?',
+  'Leo Fender, com o Precision Bass em 1951.'
+)
